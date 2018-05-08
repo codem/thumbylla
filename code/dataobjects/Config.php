e5dadd90eb5371ab49f008416e4e6846cf00d72d
James Ellis <james.ellis@dpc.nsw.gov.au>
2018-05-08 01:14:26 +0000

    Indentation fix

--- code/dataobjects/Config.php
+++ code/dataobjects/Config.php
@@ -4,8 +4,8 @@ namespace Codem\Thumbor;
  * Simple Config class for Thumbor
  */
 class Config extends \Object {
-  private static $thumbor_generation_key = '';
-  private static $backend_protocol_https = false;
-  private static $backend_path = '';
-	private static $backends = [];
+    private static $thumbor_generation_key = '';
+    private static $backend_protocol_https = false;
+    private static $backend_path = '';
+    private static $backends = [];
 }
