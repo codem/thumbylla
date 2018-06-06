7373e105a519c230093e4e02e1ee97afad4109d1
James Ellis <james.ellis@dpc.nsw.gov.au>
2018-06-06 16:00:24 +1000

    mv public assets to public, expose in composer, update Requirements

--- src/fields/ManualCropField.php
+++ src/fields/ManualCropField.php
@@ -35,16 +35,13 @@ class ManualCropField {
 
 	public function getField() {
 
-		$path = realpath(dirname(__FILE__) . '/../../');
-		$base = basename($path);
-
 		if(!empty($this->image->ID) && $this->image instanceof SS_Image) {
 
 			$crop_data = $this->image->ManualCropData;//need to call onCropStart on this ?
 			// Load necessary scripts and styles
-			Requirements::javascript($base . '/lib/cropperjs/dist/cropper.min.js');
-			Requirements::javascript($base . '/javascript/manualcrop.js');
-			Requirements::css($base . '/lib/cropperjs/dist/cropper.min.css');
+			Requirements::javascript("codem/thumbylla:public/lib/cropperjs/dist/cropper.min.js");
+			Requirements::javascript("codem/thumbylla:public/javascript/manualcrop.js");
+			Requirements::css("codem/thumbylla:public/lib/cropperjs/dist/cropper.min.css");
 
 			$id = $this->image->ID;
 			$field_id = "manualcrop-{$id}";
