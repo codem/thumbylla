e3c3f900f15e01d80230a8e9c08f17d3a00f5431
James Ellis <james.ellis@dpc.nsw.gov.au>
2018-06-19 06:26:03 +0000

    If the image is in the fileSelectForm do not display crop tab as there is no way to save it

--- src/Extensions/AssetFormFactoryExtension.php
+++ src/Extensions/AssetFormFactoryExtension.php
@@ -3,16 +3,23 @@ namespace Codem\Thumbor;
 use SilverStripe\Core\Extension;
 use SilverStripe\Assets\Image As SS_Image;
 use SilverStripe\Forms\Fieldlist;
+use SilverStripe\Forms\LiteralField;
 
 /**
  * This {@link \Extension} provides storage for manual cropping via {@link Codem\Thumbor\ManualCropField}
  */
 class AssetFormFactoryExtension extends Extension {
+
+
   /**
 	 * CMS Fields
 	 * @return FieldList
 	 */
 	public function updateFormFields($fields, $controller, $formName, $context) {
+		if($formName == 'fileSelectForm') {
+			// cannot edit crops in select mode - there are no save/publish buttons
+			return;
+		}
 		$record = isset($context['Record']) ? $context['Record'] : null;
 		if(!$record instanceof SS_Image) {
 			return;
